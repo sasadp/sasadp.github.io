1c0ffc5ff8e50c3e3aa83437ccc29de9f897281b
sasadp <priskadilahsalsa@gmail.com>
2024-01-11 09:34:11 +0800

    first commit

---
+++ Asset/aqis.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Mendapatkan elemen audio
+    var audio = document.getElementById("birthdayAudio");
+    // Menambahkan event listener untuk memulai audio saat pengguna berinteraksi
+    document.addEventListener('click', function() {
+    audio.play();
+    });
+  });
+  
+  function playBirthdaySong() {
+    // Display birthday messages
+    alert("Selamat ulang tahun, sist! 🎉🎂");
+    alert("Bahagia terus!");
+    alert("Seimbangi dunia dan akhiratnya ya.");
+    alert("Kalau cape, istirahat.");
+    alert("Jangan berharap banyak sama manusia gengsi.");
+    alert("Emang gitu, sabar aja kalo temenan sama aku");
+    alert("Kadonya ntar aja ya kalo aku udah kerja, hehe sowryyy be");
+    alert("20 tahun jadi orang yang enakan aja");
+    alert("Jangan mikirin perasaan orang terus");
+    alert("Jangan sering mikirin aku juga wahahahah");
+    alert("Aku always sehat, insya Allah");
+    alert("Aku buat ini karna ada ide aja sih, dibantu AI kok jadi jangan terkesima");
+    alert("Aku pede aja dulu yakan, siapa tau kamu beneran terkesima");
+    alert("Udah ah, aku lagi introvert");
+    alert("Kamu tua, ya :p");
+    alert("Lagi ah, Kamu tua ya :p");
+    alert("Sekali lagi, Kamu tua ya :p");
+    alert("TUAAAAAAAAA :p");
+    alert("Gpp be, pacarmu tambah sayang kok");
+    alert("Udah ah, aku lagi introvert part 2");
+    alert("Udah be, sekian, thankyou imnida.");
+    alert("Sekian dari Salsa Dilah Priska yang cantik nan rajin menabung ini. <333333")
+  }
+  
